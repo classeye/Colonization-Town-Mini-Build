d9e52034d10f64e8a513caae348b956c60d4adea
classeye <vinceytang@gmail.com>
2017-02-05 16:26:28 -0800

    Initial Commit
    Map generation and early events have been implemented.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;       //Allows us to use Lists.
+using Random = UnityEngine.Random;      //Tells Random to use the Unity Engine random number generator.
+
+public class BoardManager : MonoBehaviour
+{
+    public int columns = 7;
+    public int rows = 7;
+    public GameObject[] Tiles; //an array loaded with world tile objects
+    public GameObject townTile; // the variable the town object will be stored in
+    private Transform boardHolder; //variable for the transform of the board
+    private string tileID;
+
+
+    public void CreateBoard() //called by GameManager
+    {
+        boardHolder = new GameObject("Board").transform; // set boardholder to board transform
+
+        int tileNum = new int();
+        tileNum = 0;
+
+        //once for every column
+        for (int x = 0; x < columns; x++)
+        {
+            //and once for every row
+            for (int y = 0; y < rows; y++)
+            {
+                tileNum++;
+
+                //instantiate a tile from the world tiles list
+                GameObject toInstantiate = Tiles[Random.Range(0, Tiles.Length)];
+                //unless it is tile 3,3. in which case load the town tile
+                if (x == 3 && y == 3)
+                {
+                toInstantiate = townTile;
+                }
+                //the instantiate that chosen object at row/column in this for loop
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
+                //and parent it to boardholder
+                instance.transform.SetParent(boardHolder);
+                //instance.gameObject.name = "Tile" + tileID;
+                if (tileNum < 10)
+                {
+                    tileID = "0" + tileNum.ToString();
+                }
+                else
+                {
+                    tileID = tileNum.ToString();
+                }
+
+
+                instance.gameObject.name = "Tile" + tileID;
+            }
+         
+                
+                    
+   
+
+            }
+        }
+    }
+
+  
+  
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private BoardManager boardScript;
+
+    void Awake()
+    {
+       //attach variable to BoardManager script
+        boardScript = GetComponent<BoardManager>();
+        InitiateGame();
+    }
+
+    void InitiateGame()
+    {
+        boardScript.CreateBoard();
+    }
+
+}
